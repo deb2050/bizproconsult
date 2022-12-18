b9ed16d349a0a84efe985a5668a0e29212b10509
deb2050 <debparna@gmail.com>
2022-12-18 22:31:14 +0100

    add our testitominals and contact components

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Contact = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col sm={12}>1 of 1</Col>
+                </Row>
+                <hr />
+            </Container>
+        </footer>
+    )
+}
---
+++ src/components/OurTestimonials.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import './OurTestimonials.css';
+
+const OurTestimonials = () => {
+    return (
+        <section>
+            <Container>
+            <h2>Our Testimonials</h2>
+                <Row>
+                    <Col sm={12}>1 of 1</Col>
+                </Row>
+                <hr />
+            </Container>
+        </section>
+    )
+}
+
+export default OurTestimonials;
