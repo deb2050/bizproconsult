165c2a805f9eb60e0151515edfc785613007bd65
deb2050 <debparna@gmail.com>
2022-12-18 23:49:43 +0100

    shifting content to App js

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,41 @@ import Footer from './common/Footer';
 
 import './App.css';
 
+const servicesData = [
+  {
+      id: '1',
+      heading: 'Finanzberatung',
+      serviceText: 'Unsere Dienstleistungen sind darauf ausgerichtet, Sie über alle Aspekte und Auswirkungen einer Transaktion zu informieren, Sie von administrativem Aufwand zu befreien und Ihnen zum richtigen Zeitpunkt den richtigen Rat zu geben'
+  },
+  {
+      id: '2',
+      heading: 'Strategie-Beratung',
+      serviceText: 'Nahtlose Visualisierung von hochwertigem intellektuellem Kapital ohne überragende Zusammenarbeit und auf Ideenaustausch ausgerichtete Konvergenz.'
+  },
+  {
+      id: '3',
+      heading: 'HR-Beratung',
+      serviceText: 'Dynamische Innovation des ressourcenbasierten Kundendienstes für eine plattformübergreifende Integration des Kundendienstes auf dem neuesten Stand der Technik.'
+  },
+  {
+      id: '4',
+      heading: 'Organisationsstruktur',
+      serviceText: 'Nahtlose Ermöglichung vollständig erforschter Wachstumsstrategien und interoperabler interner oder organischer, ressourcenorientierter Kundendienste.'
+  },
+  {
+      id: '5',
+      heading: 'Strategie',
+      serviceText: 'Das strategische Management umfasst die Formulierung von Strategien und Zielen für das gesamte Unternehmen und die einzelnen Geschäftsbereiche.'
+  }
+]
+
 function App() {
   return (
     <div className="App">
       <Header />
       <CarouselBanner />
       <About />
-      <OurServices />
+      <OurServices servicesData={servicesData} />
       <OurTeam />
       <OurTestimonials />
       <Contact />
--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -8,35 +8,9 @@ import './OurServices.css';
 import about from '../images/about.jpg';
 
 const serviceMainText = 'Mit unserem Ansatz, niedrig hängende Früchte zu nutzen, um eine ungefähre Aktivität mit Mehrwert für einen Betatest zu identifizieren, können wir die Arbeit immer erledigen. Überwinden Sie die digitale Kluft mit zusätzlichen Klickzahlen.';
-const servicesData = [
-    {
-        id: '1',
-        heading: 'Finanzberatung',
-        serviceText: 'Unsere Dienstleistungen sind darauf ausgerichtet, Sie über alle Aspekte und Auswirkungen einer Transaktion zu informieren, Sie von administrativem Aufwand zu befreien und Ihnen zum richtigen Zeitpunkt den richtigen Rat zu geben'
-    },
-    {
-        id: '2',
-        heading: 'Strategie-Beratung',
-        serviceText: 'Nahtlose Visualisierung von hochwertigem intellektuellem Kapital ohne überragende Zusammenarbeit und auf Ideenaustausch ausgerichtete Konvergenz.'
-    },
-    {
-        id: '3',
-        heading: 'HR-Beratung',
-        serviceText: 'Dynamische Innovation des ressourcenbasierten Kundendienstes für eine plattformübergreifende Integration des Kundendienstes auf dem neuesten Stand der Technik.'
-    },
-    {
-        id: '4',
-        heading: 'Organisationsstruktur',
-        serviceText: 'Nahtlose Ermöglichung vollständig erforschter Wachstumsstrategien und interoperabler interner oder organischer, ressourcenorientierter Kundendienste.'
-    },
-    {
-        id: '5',
-        heading: 'Strategie',
-        serviceText: 'Das strategische Management umfasst die Formulierung von Strategien und Zielen für das gesamte Unternehmen und die einzelnen Geschäftsbereiche.'
-    },
-]
 
-const OurServices = () => {
+
+const OurServices = (props) => {
     return (
         <section>
             <Container>
@@ -48,7 +22,7 @@ const OurServices = () => {
                         </p>
                     </Col>
                     {
-                        servicesData.map((service) => {
+                        props.servicesData.map((service) => {
                             return (
                                 <Col sm={4} key={service.id}>
                                 <Figure>
